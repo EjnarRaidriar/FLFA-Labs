f67b500d642a8ec696102160c6eaf5f99e28946e
EjnarRaidriar <emanuil.cucos25@gmail.com>
2023-02-21 12:37:54 +0200

    Added report

---
+++ Lab 1/src/main.py
@@ -0,0 +1,19 @@
+from Grammar import Grammar
+from Automaton import Automaton
+
+G = Grammar.Grammar(['S', 'B', 'D'],
+                    ['a', 'b', 'c', 'd'],
+                    {
+                'S': ['aS', 'bB'],
+                'B': ['cB', 'd', 'aD'],
+                'D': ['aB', 'b']
+            },
+            'S')
+
+word = G.generateString()
+print(word)
+A = Automaton.FiniteAutomaton([], [], {}, "", [])
+A = Automaton.FiniteAutomaton.RG_to_NFA(A, G)
+if A.check_word(word):
+    print("check")
+
